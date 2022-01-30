4cd18d6b36279883ed5c6001f52c74cdf5714372
younghwani <younghwankim624@gmail.com>
2022-01-30 16:10:26 +0900

    코드 정리 및 스타일 변경

--- src/container/Main.js
+++ src/container/Main.js
@@ -21,7 +21,7 @@ const Main = () => {
 	// TODO: 단어 수 세는 로직 필요하다.
 	const updateTextLen = React.useCallback(() => {
 		document.querySelector(
-			'.label__right'
+			'.text_len'
 		).textContent = `글자 수 : ${input.length}`;
 	}, [input]);
 
@@ -29,7 +29,7 @@ const Main = () => {
 		updateTextLen();
 	}, [updateTextLen]);
 
-	const handleSubmitKor = async (event) => {
+	const handleSubmit = async (event) => {
 		event.preventDefault();
 		setIsLoading(true);
 		const data = await axios.get(
@@ -56,60 +56,47 @@ const Main = () => {
 	}
 
 	return (
-		<div className="mainContainer">
-			<div className="contentsContainer">
-				<Header />
-				{isLoading ? (
-					<div className="runningMsg">
-						<h2>요약 중입니다.</h2>
-						<p>잠시 후 결과창으로 이동합니다.</p>
-					</div>
-				) : (
-					<Container className="formContainer">
-						<br />
-						<div>
-							<Form onSubmit={handleSubmitKor}>
-								<Label for="input" className="label">
-									<div className="label__left">
-										요약할 텍스트를 입력해주세요. 500 단어
-										이내로 입력하세요!
-									</div>
-									<div className="label__right">
-										텍스트 길이가 표시됩니다.
-									</div>
-									<Button
-										color="primary"
-										onClick={btnRefresh}
-									>
-										새로고침
-									</Button>
-								</Label>
-								<br />
-								<textarea
-									className="input__text"
-									type="text"
-									name="input"
-									id="input"
-									value={input}
-									onChange={onInputChange}
-									placeholder="텍스트를 입력해주세요.."
-									onKeyPress={handleKeyPress}
-								/>
-								<br />
-								<br />
-								<FormGroup className="resultBtn">
-									<Button color="success" type="submit">
-										요약하기
-									</Button>{' '}
-								</FormGroup>
-							</Form>
+		<div className="contentsContainer">
+			<Header />
+			<Container className="formContainer">
+				<Form onSubmit={handleSubmit}>
+					<Label for="input" className="label">
+						<div className="label__desc">
+							요약할 텍스트를 입력해주세요. 500 단어 이내로
+							입력하세요!
 						</div>
-						<br />
-						<br />
-					</Container>
-				)}
-				<Footer />
-			</div>
+						<Button color="primary" onClick={btnRefresh}>
+							새로고침
+						</Button>
+					</Label>
+					<br />
+					<textarea
+						className="input__text"
+						type="text"
+						name="input"
+						id="input"
+						value={input}
+						onChange={onInputChange}
+						placeholder="텍스트를 입력해주세요.."
+						onKeyPress={handleKeyPress}
+					/>
+					<div className="form__footer">
+						<div className="text_len">
+							텍스트 길이가 표시됩니다.
+						</div>
+						{isLoading ? (
+							<p>요약 중입니다.</p>
+						) : (
+							<FormGroup className="resultBtn">
+								<Button color="success" type="submit">
+									요약하기
+								</Button>{' '}
+							</FormGroup>
+						)}
+					</div>
+				</Form>
+			</Container>
+			<Footer />
 		</div>
 	);
 };
